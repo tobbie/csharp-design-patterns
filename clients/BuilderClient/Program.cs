014e3912bab95018d8c04f136c84a8d52a247af2
tobbie <tobi.akinseye@gmail.com>
2025-06-29 23:32:13 +0100

    added builder pattern class library and client

---
+++ clients/BuilderClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Builder;
+
+
+Console.Title = "Builder";
+
+var garage  = new Garage();
+
+var miniBuilder = new MiniBuilder();
+var bmwBuilder = new BMWBuilder();
+
+garage.Construct(miniBuilder);
+Console.WriteLine(miniBuilder.Car.ToString());
+// or:
+garage.Show();
+
+garage.Construct(bmwBuilder);
+Console.WriteLine(bmwBuilder.Car.ToString());
+//or:
+garage.Show();
+
+Console.ReadKey();
---
+++ src/Builder/Implementation.cs
@@ -0,0 +1,127 @@
+﻿using System.Text;
+
+namespace Builder;
+
+/**
+*****Builder Pattern Components******
+ * 1. Builder
+ * 2. ConcreteBuilderA
+ * 3. ConcreteBuilderB
+ * 4. Product (e.g car)
+ * 5. Director --- uses abstract builder to construct car representations
+**/
+
+
+
+/// <summary>
+/// Product - Object created by builder
+/// </summary>
+
+public class Car
+{
+    private readonly string _carType;
+    private readonly List<string> _parts = new();
+    public Car(string carType)
+    {
+        _carType = carType;
+    }
+
+    public void AddPart(string part)
+    { 
+        _parts.Add(part);
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        foreach (string part in _parts) {
+            sb.Append($" Car of type {_carType} has part {part}.  ");
+        }
+        return sb.ToString();
+    }
+}
+
+
+/// <summary>
+/// Builder
+/// </summary>
+public abstract class CarBuilder
+{
+    public abstract void BuildEngine();
+    public abstract void BuildFrame();
+
+    public Car Car { get; private set; }
+
+    public CarBuilder(string carType) { 
+        Car = new Car(carType);
+    }
+}
+
+
+/// <summary>
+/// ConcreteBuilderA
+/// </summary>
+public class MiniBuilder : CarBuilder
+{
+    public MiniBuilder() : base("Mini")
+    {
+    }
+
+    public override void BuildEngine()
+    {
+        Car.AddPart("'not a V8'");
+    }
+
+    public override void BuildFrame()
+    {
+        Car.AddPart("'3-door with stripes'");
+    }
+}
+
+
+/// <summary>
+/// ConcreteBuilderB
+/// </summary>
+public class BMWBuilder : CarBuilder
+{
+    public BMWBuilder() : base("BMW")
+    {
+    }
+
+    public override void BuildEngine()
+    {
+        Car.AddPart("'a fancy V8 engine'");
+    }
+
+    public override void BuildFrame()
+    {
+        Car.AddPart("'5-door with metallic finish'");
+    }
+}
+
+/// <summary>
+/// Director - uses abstract builder class to build the required car representation
+/// abstract builder class can be introduced by construction injection or via a construct method
+/// </summary>
+public class Garage
+{
+    private CarBuilder? _builder;
+    public Garage() { }
+
+    public void Construct(CarBuilder builder)
+    {
+        _builder = builder;
+        _builder.BuildEngine();
+        _builder.BuildFrame();
+    }
+
+    public void Show()
+    {
+        Console.WriteLine(_builder?.Car.ToString());
+    }
+}
+
+
+
+
+
