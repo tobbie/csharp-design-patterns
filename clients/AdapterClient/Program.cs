8a3dfefd6252dfa451085633539a845945cf39d7
tobbie <tobi.akinseye@gmail.com>
2025-07-07 21:10:41 +0100

    added code to demonstrate the object adapter pattern

---
+++ clients/AdapterClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using Adapter;
+using static System.Console;
+
+Title = "Adapter Pattern";
+
+ICityAdapter adapter   =  new CityAdapter();
+var city = adapter.GetCity();
+
+WriteLine($"{city.FullName}, {city.Inhabitants}");
+
+ReadKey();
+
+
+
+
+
