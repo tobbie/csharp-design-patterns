223392ba1091087060d44ca1798db8a03e92f1ad
tobbie <tobi.akinseye@gmail.com>
2025-07-13 21:04:37 +0100

    added client code to consume strategy pattern

---
+++ clients/ConsoleClient/Examples/StrategyDemo.cs
@@ -0,0 +1,31 @@
+﻿using static  System.Console;
+using Strategy;
+
+namespace ConsoleClient.Examples
+{
+    static class StrategyDemo
+    {
+        public static void Run() 
+        {
+            Title = "The Strategy Pattern";
+            var order = new Order("Oluwatobi Software", "Visual Studio License", 200);
+            order.ExportService = new CsvExportService();
+            order.Export();
+
+            order.ExportService = new JsonExportService();
+            order.Export();
+
+            order.ExportService = new XmlExportService();
+            order.Export();
+
+            ReadKey();  
+
+
+
+
+
+
+        }
+
+    }
+}
