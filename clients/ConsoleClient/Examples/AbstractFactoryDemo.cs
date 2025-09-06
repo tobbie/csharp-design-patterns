1af2e2c2d8de81e8fee056bbf9ca89dd7c2926d3
tobbie <tobi.akinseye@gmail.com>
2025-09-06 15:13:24 +0100

    added code to demonstrate the abstract factory from a client

---
+++ clients/ConsoleClient/Examples/AbstractFactoryDemo.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+using AbstractFactory;
+
+namespace ConsoleClient.Examples
+{
+    static class AbstractFactoryDemo
+    {
+        public static void Run()
+        {
+            Title = "Abstract Factory Demo";
+
+            var belgiumShoppingCartPurchaseFactory = new BelgiumShoppingCartPurchaseFactory();
+            var shoppingCartForBelgium  = new ShoppingCart(belgiumShoppingCartPurchaseFactory);
+            shoppingCartForBelgium.CalculateCosts();
+
+            var franceShoppingCartPurchaseFactory = new FranceShoppingCartPurchaseFactory();
+            var shoppingCartForFrance = new ShoppingCart(franceShoppingCartPurchaseFactory);
+            shoppingCartForFrance.CalculateCosts();
+
+            ReadKey();
+
+        }
+    }
+}
